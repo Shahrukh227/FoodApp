4aa80c4ecb64ab8e7bb03846932cdede3a34e9df
Shahrukh227 <shahru0005@gmail.com>
2024-10-07 02:08:11 +0530

    home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import CoverPage from '../Components/CoverPage'
-import Footer from '../Components/Footer'
+
 
 function Home() {
   return (
    <main>
     <Navbar/>
     <CoverPage/>
-    <Footer/>
+    
    </main>
   )
 }
